e04b074f08a85ec2a88c13f2626587842d938ef2
Aviv Elbag <aviv.elbag@cognite.com>
2019-08-10 15:01:46 +0200

    did another refactor

--- Finance/Data.py
+++ Finance/Data.py
@@ -5,8 +5,17 @@ import pytz
 from .Plots import CandleStick
 from .Indicator import simple_moving_average
 
+
 class StockData:
-    def __init__(self,ticker,period_type="DAY",number_of_periods=10,frequency_type="MINUTE",number_of_frequency=5,timezone = "US/Eastern"):
+    def __init__(
+        self,
+        ticker,
+        period_type="DAY",
+        number_of_periods=10,
+        frequency_type="MINUTE",
+        number_of_frequency=5,
+        timezone="US/Eastern",
+    ):
 
         self.ticker = ticker
         self.my_share = share.Share(self.ticker)
@@ -17,28 +26,30 @@ class StockData:
         if frequency_type == "MINUTE":
             self.frequency_type = share.FREQUENCY_TYPE_MINUTE
 
-
-        self.symbol_data = self.my_share.get_historical(self.period_type,
-                                              number_of_periods,
-                                              self.frequency_type,
-                                              number_of_frequency)
+        self.symbol_data = self.my_share.get_historical(
+            self.period_type, number_of_periods, self.frequency_type, number_of_frequency
+        )
 
         self.timezone = pytz.timezone(timezone)
 
-        self.dates = [pytz.timezone('UTC').localize(pytz.datetime.datetime.fromtimestamp(time_stamp/1000)).astimezone(self.timezone) for time_stamp in
-                 self.symbol_data['timestamp']]
+        self.dates = [
+            pytz.timezone("UTC")
+            .localize(pytz.datetime.datetime.fromtimestamp(time_stamp / 1000))
+            .astimezone(self.timezone)
+            for time_stamp in self.symbol_data["timestamp"]
+        ]
 
-        self.close = self.symbol_data['close']
-        self.open = self.symbol_data['open']
-        self.high = self.symbol_data['high']
-        self.low = self.symbol_data['low']
-        self.volume = self.symbol_data['volume']
+        self.close = self.symbol_data["close"]
+        self.open = self.symbol_data["open"]
+        self.high = self.symbol_data["high"]
+        self.low = self.symbol_data["low"]
+        self.volume = self.symbol_data["volume"]
 
     def plot_candle_stick(self):
         plot = CandleStick(self)
         plot.show()
 
-    def moving_average(self,window=10,based_on="close"):
+    def moving_average(self, window=10, based_on="close"):
         if based_on == "close":
             data = self.close
         elif based_on == "open":
@@ -47,11 +58,4 @@ class StockData:
             print("The data doesn't exist")
             return
 
-        return simple_moving_average(data,window)
-
-
-
-
-
-
-
+        return simple_moving_average(data, window)
--- Finance/Indicator.py
+++ Finance/Indicator.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-def simple_moving_average(data,window=10):
+
+def simple_moving_average(data, window=10):
     sma = np.cumsum(data)
-    sma = sma[window:]-sma[:-window]
-    return sma/window
+    sma = sma[window:] - sma[:-window]
+    return sma / window
 
 
 def relative_strength_index(stockData):
