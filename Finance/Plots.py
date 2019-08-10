adb417f57a6dcac53c7de7c75d207e49a0db5e0e
Aviv Elbag <aviv.elbag@cognite.com>
2019-08-10 15:00:47 +0200

    small refactor

--- Finance/Plots.py
+++ Finance/Plots.py
@@ -1,3 +1,9 @@
 import plotly.graph_objects as go
 def CandleStick(stockData):
-    return go.Figure(data =go.Candlestick(x=stockData.dates,close=stockData.close,open=stockData.open,low=stockData.low,high = stockData.high))
+    candle_stick = go.Candlestick(x=stockData.dates,
+                                  close=stockData.close,
+                                  open=stockData.open,
+                                  low=stockData.low,
+                                  high = stockData.high)
+    output_figure = go.Figure(data =candle_stick)
+    return output_figure
